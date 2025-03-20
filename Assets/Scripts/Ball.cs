bffc1e694bea3ac1ad47b21a110510cdd4dc82ad
Chaniel M. <chanieljm@gmail.com>
2025-03-20 14:35:23 -0400

    Destroy Ball Upon 20 Bounces

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -3,16 +3,16 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    int bounces;
+    Rigidbody2D rb;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        Rigidbody2D rb = GetComponent<Rigidbody2D>();
-        Vector3 position = transform.position;
-        position.x = 2;
-        transform.position = position;
-        transform.localScale *= 2;
+    void Start() {
+        bounces = 0;
         
-        float mag = Random.Range(3, 5);
+        rb = GetComponent<Rigidbody2D>();
+        transform.localScale *= 2;
+        float mag = Random.Range(3, 10);
         float dir = Random.Range(0, 2 * Mathf.PI);
         Vector2 force = new Vector2(Mathf.Cos(dir), Mathf.Sin(dir)) * mag;
         Debug.Log($"Magnitude: {mag}, Direction: {dir*180/Mathf.PI}*");
@@ -20,8 +20,15 @@ public class Ball : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if (bounces > 20) {
+            Destroy(gameObject);
+            Debug.Log("Bang!");
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        bounces++;
+        Debug.Log($"Bounces: {bounces}");
     }
 }
